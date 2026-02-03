37a40906e115ccbfa4b868da9661c6f413b948a0
lvliangxiao <14692774+lvliangxiao@user.noreply.gitee.com>
2026-02-03 23:42:54 +0800

    添加退出通讯录功能

--- main.cpp
+++ main.cpp
@@ -6,11 +6,54 @@ using namespace std;
 
 void showMenu()
 {
-
+    cout << "*********************" << endl;
+    cout << "*****1. 添加联系人*****" << endl;
+    cout << "*****2. 显示联系人*****" << endl;
+    cout << "*****3. 删除联系人*****" << endl;
+    cout << "*****4. 查找联系人*****" << endl;
+    cout << "*****5. 修改联系人*****" << endl;
+    cout << "*****6. 清空联系人*****" << endl;
+    cout << "*****0. 退出通讯录*****" << endl;
+    cout << "*********************" << endl;
 }
+
+
+
 
 int main()
 {
+    int select = 0;
+
+    while (true) {
+
+        // 菜单调用
+        showMenu();
+        // 用户输入选择
+        cin >> select;
+        switch (select) {
+            case 1:  //1. 添加联系人
+                break;
+            case 2:  //2. 显示联系人
+                break;
+            case 3:  //3. 删除联系人
+                break;
+            case 4:  //4. 查找联系人
+                break;
+            case 5:  //5. 修改联系人
+                break;
+            case 6:  //6. 清空联系人
+                break;
+            case 0:  //0. 退出通讯录
+                cout << "欢迎下次使用" << endl;
+                system("pause");
+                return 0;
+                break;
+            default:
+                break;
+        }
+    }
+
+
     system("pause");
     return 0;
 }
